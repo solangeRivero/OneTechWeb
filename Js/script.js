eef851e35d115cbcd3b250a602e66a0e3c6c6911
Solange <solangerivero1312@gmail.com>
2024-03-08 11:35:43 -0300

    Pagina web

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+} 
